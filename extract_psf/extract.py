a125b32048f8be93561138153a5e671e7e5f18b9
Marten van Kerkwijk <mhvk@astro.utoronto.ca>
2013-06-07 15:36:40 -0400

    PEP8 on extract.py

--- extract_psf/extract.py
+++ extract_psf/extract.py
@@ -4,6 +4,7 @@ import fortran.extrpsf as extrpsf
 
 # turn output into astropy table instead?
 
+
 class PSF:
     """Point-spread function for extraction"""
 
@@ -15,24 +16,25 @@ class PSF:
         self.chi2 = []
 
     def setguess(self, guesses):
-        assert len(guesses) == (3 if self.form == 0 or self.form == -1 
+        assert len(guesses) == (3 if self.form == 0 or self.form == -1
                                 else 2+(self.form-1)*3)
         self.npoldisp = np.zeros(len(guesses),dtype=np.int)
         self.guess = np.array([], dtype=[('par',np.float), ('fix',np.bool)])
         for i,g in enumerate(guesses):
-            try: # Element(s) given in full?
+            try:  # Element(s) given in full?
                 npg = np.array(g, dtype=self.guess.dtype)
                 # Catch something like ((0.,(1.,True)),...)
                 # where the tuple would be taken to be a Boolean
                 # (strictly, this should be given as [[0.,(1.,True)],...])
-                if isinstance(g[1],tuple): raise TypeError
+                if isinstance(g[1], tuple):
+                    raise TypeError
             except (TypeError, ValueError):
-                try: # Perhaps a number with default choice of not fixed?
+                try:  # Perhaps a number with default choice of not fixed?
                     npg = np.array((g,False), dtype=self.guess.dtype)
                 except (TypeError, ValueError):  # Now it must be a list
                     npg = np.array([], dtype=self.guess.dtype)
                     for gg in g:
-                        try: # Element is given in full?
+                        try:  # Element is given in full?
                             npgg = np.array(gg, dtype=self.guess.dtype)
                         except (TypeError, ValueError):  # Just a number?
                             npgg = np.array((gg,False), dtype=self.guess.dtype)
@@ -57,7 +59,7 @@ class PSF:
         """Evaluate PSF using fitted parameters"""
         # ignore derivatives from extrpsf.psf (all zero if Normalize=True)
         val,_ = extrpsf.psfmany(ipsf=self.form, x=x,
-                                par=self.evalpar(y,useguess), 
+                                par=self.evalpar(y, useguess),
                                 normalise=normalise)
         return val
 
@@ -65,18 +67,20 @@ class PSF:
         if len(self.par) == len(self.guess):
             return self.par[0]
         else:
-            return np.vstack([self.par[0], 
+            return np.vstack([self.par[0],
                               self.par[0]+self.par[len(self.guess):]])
+
 
 def ccd_noise(d, ron, fferr):
     return np.sqrt(np.abs(d) + ron*ron + (fferr*d)**2)
 
-def extract(d, tracepos, psf, 
-            skypol=2, fitrange=None, clip=[5.,2.], 
+
+def extract(d, tracepos, psf,
+            skypol=2, fitrange=None, clip=[5.,2.],
             e=None, ron=4., fferr=0.03,
             spatial='y', itesttype=103, ibadlimit=3, squeeze_dims=True):
     """Extract spectrum by fitting sky and PSF to stellar traces"""
-    assert d.ndim <= 3 # should be at most stacks of images
+    assert d.ndim <= 3  # should be at most stacks of images
     # estimate of uncertainties (if not passed on)
     if e is None:
         e = ccd_noise(d, ron, fferr)
@@ -92,7 +96,7 @@ def extract(d, tracepos, psf,
         else:
             ndisp, nspat = d.shape[-2:]
             istep_d = np.array([1,nspat])
-        
+
     # Define the "trace" of the star, offset by a chip size for each "order"
     # and defined for every pixel in the dispersion direction
     # (used to trace curved orders in echelle spectra)
@@ -115,7 +119,7 @@ def extract(d, tracepos, psf,
             psfguess = np.hstack([psfguess, tracepos[1:]-tracepos[0]])
             ifit = np.hstack([ifit, np.ones(nstar-1,dtype=int)])
             npoldisp = np.hstack([npoldisp, np.zeros(nstar-1,dtype=int)])
-    xord += tracepos[0] + 1 # fortran is 1-based
+    xord += tracepos[0] + 1  # fortran is 1-based
     # Define physical size of the chip
     # For multiple, use whole stack with different orders as a hack.
     kbeg = 1
@@ -161,33 +165,35 @@ def extract(d, tracepos, psf,
         d.shape = e.shape = test.shape = d_orig_shape
         # Set output array shape, removing unnecessary dimensions if wanted
         out.shape = eout.shape = back.shape = chi2.shape = [
-            n for n in (nstar, norder, ndisp) if n>1 or not squeeze_dims ]
+            n for n in (nstar, norder, ndisp) if n > 1 or not squeeze_dims]
 
-        return (out, eout, back, chi2, test, 
+        return (out, eout, back, chi2, test,
                 ntdiscard, ntbadl, ntbadh, nproblems)
 
     else:
-        _,_,_,_,_, back, chi2, test, _, ntbadl, ntbadh, _ = extrpsf.extract_psf(
-            d, istep_in=istep_d, ein=e, istart_ein=0, istep_ein=istep_d,
-            ipbeg=ipbeg, ipend=ipend,
-            kbeg=kbeg, kend=kend, lbeg=lbeg, lend=lend,
-            xord=xord, tilt1=tilt1, tilt2=tilt1, tilt_flag=False, npoltilt=0,
-            mindof=0, nitermax=0, clip1=clip, clip2=0.,
-            ipsf=0, psfguess=np.array([0.]), ifix=np.array([0]),
-            npoldisp=np.array([0]), nshape=1,
-            nstar=0, npolsky=skypol,
-            nout=1, nback=nout, nchi2=nout, istep_out=istep_out,
-            ntest=d.size, istart_test=0, istep_test=istep_d,
-            o_flag=False, e_flag=False, b_flag=True, c_flag=True,
-            p_flag=False, s_flag=False,
-            itesttype=itesttype, rnull=0., ibadlimit=2)
+        (_,_,_,_,_, back, chi2, test,
+         _, ntbadl, ntbadh, _) = extrpsf.extract_psf(
+             d, istep_in=istep_d, ein=e, istart_ein=0, istep_ein=istep_d,
+             ipbeg=ipbeg, ipend=ipend,
+             kbeg=kbeg, kend=kend, lbeg=lbeg, lend=lend,
+             xord=xord, tilt1=tilt1, tilt2=tilt1, tilt_flag=False, npoltilt=0,
+             mindof=0, nitermax=0, clip1=clip, clip2=0.,
+             ipsf=0, psfguess=np.array([0.]), ifix=np.array([0]),
+             npoldisp=np.array([0]), nshape=1,
+             nstar=0, npolsky=skypol,
+             nout=1, nback=nout, nchi2=nout, istep_out=istep_out,
+             ntest=d.size, istart_test=0, istep_test=istep_d,
+             o_flag=False, e_flag=False, b_flag=True, c_flag=True,
+             p_flag=False, s_flag=False,
+             itesttype=itesttype, rnull=0., ibadlimit=2)
         # Restore proper shapes of input arrays
         d.shape = e.shape = test.shape = d_orig_shape
         # Set output array shape, removing unnecessary dimensions
-        back.shape = chi2.shape = [n for n in (norder, ndisp) 
-                                   if n>1 or not squeeze_dims]
+        back.shape = chi2.shape = [n for n in (norder, ndisp)
+                                   if n > 1 or not squeeze_dims]
 
         return (back, chi2, test, ntbadl, ntbadh)
+
 
 def fitsky(d, skypol=2, clip=5., itesttype=101, **kwargs):
     return extract(d, skypol=skypol, clip=clip, itesttype=itesttype,
